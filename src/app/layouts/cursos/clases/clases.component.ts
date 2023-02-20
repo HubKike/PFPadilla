628750b00e3f3467a246f335026bbbe76d9253f8
Enrique Padilla <ultrared808@gmail.com>
2023-02-19 23:31:32 -0600

    Actualizacion 190223

--- src/app/layouts/cursos/clases/clases.component.ts
+++ src/app/layouts/cursos/clases/clases.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-clases',
   templateUrl: './clases.component.html',
   styleUrls: ['./clases.component.scss']
 })
-export class ClasesComponent {
+export class ClasesComponent implements OnInit {
+
+  detalleAlumnos!: FormArray<any>;
+
+  constructor(private builder: FormBuilder) { }
+
+  formularioClase = this.builder.group({
+    cursoClave: this.builder.control('', Validators.required),
+    cursoId: this.builder.control('', Validators.required),
+    cursoNombre: this.builder.control(''),
+    instructorId: this.builder.control('', Validators.required),
+    instructor: this.builder.control(''),
+    alumnos: this.builder.array([])
+  });
+
+  ngOnInit(): void {
+  }
+
+  // addnewproduct
+  agregarAlumno() {
+    this.detalleAlumnos = this.formularioClase.get("alumnos") as FormArray;
+    this.detalleAlumnos.push(this.insertarFila());
+  }
+
+  // Generaterow()
+  insertarFila() {
+    return this.builder.group({
+      cursoClave: this.builder.control(''),
+      alumnoId: this.builder.control('', Validators.required),
+      alumnoNombre: this.builder.control(''),
+      alumnoApellido: this.builder.control(''),
+    })
+  }
+
+  // customerchange(){}
+  alCambiarCurso() { }
+
+  alCambiarInstructor() { }
+
+  // SaveInvoice(){}
+  guardarCurso() {
+    console.log(this.formularioClase.value);
+  }
 
 }
